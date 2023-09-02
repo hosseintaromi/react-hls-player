7e44448a831969fb1ae42cab7fa7db6517a99a81
hossein taromi <hosseintaromii@gmail.com>
2023-09-02 12:26:20 +0330

    update test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
 
-describe('Thing', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('calculate', function () {
+  it('add', function () {
+    let result = 2 + 5;
+    expect(result).toEqual(7);
   });
 });
